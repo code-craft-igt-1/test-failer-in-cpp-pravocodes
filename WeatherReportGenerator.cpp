2ba46aec1d2c61c147d660cd954b52c817d80338
Praveen Kumar <Praveen.Kumar_4@philips.com>
2024-08-20 14:02:52 +0530

    workflow

--- WeatherReportGenerator.cpp
+++ WeatherReportGenerator.cpp
@@ -2,15 +2,20 @@
 
 std::string Report(const IWeatherSensor& sensor) {
     int precipitation = sensor.Precipitation();
-    // precipitation < 20 is a sunny day
-    std::string report = "Sunny day";
+    int windSpeed = sensor.WindSpeedKMPH();
+    double temperature = sensor.TemperatureInC();
 
-    if (sensor.TemperatureInC() > 25) {
-        if (precipitation >= 20 && precipitation < 60) {
-            report = "Partly cloudy";
-        } else if (sensor.WindSpeedKMPH() > 50) {
-            report = "Alert, Stormy with heavy rain";
+    if (temperature > 25) {
+        if (precipitation >= 60) {
+            if (windSpeed > 50) {
+                return "Alert, Stormy with heavy rain";
+            } else {
+                return "Rainy day";
+            }
+        } else if (precipitation >= 20) {
+            return "Partly cloudy";
         }
     }
-    return report;
+
+    return "Sunny day";
 }
