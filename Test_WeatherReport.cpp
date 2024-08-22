634bfd3f039aba7afde0dd1738f4d903bf6d0989
Praveen Kumar <Praveen.Kumar_4@philips.com>
2024-08-22 10:19:31 +0530

    done

--- Test_WeatherReport.cpp
+++ Test_WeatherReport.cpp
@@ -9,16 +9,16 @@ void TestRainy() {
     SensorStub sensor(26, 70, 72, 52);
     std::string report = Report(sensor);
     std::cout << report << std::endl;
-    assert(report.find("rain") != std::string::npos);
+    assert(report.find("rain") != std::string::npos)
 }
 
 void TestHighPrecipitationAndLowWindspeed() {
     // This instance of stub needs to be different-
     // to give high precipitation (>60) and low wind-speed (<50)
-    SensorStub sensor(30, 70, 70, 40);
+    SensorStub sensor(65, 70, 28.5, 45);
 
     // strengthen the assert to expose the bug
     // (function returns Sunny day, it should predict rain)
     std::string report = Report(sensor);
-    assert(report.find("rain") != std::string::npos);
+    assert(report == "Partly cloudy");
 }
