d0fa18c0805f4572bbd1048d478e91ac2c57f3c3
Praveen Kumar <Praveen.Kumar_4@philips.com>
2024-08-22 09:53:19 +0530

    done

--- Test_WeatherReport.cpp
+++ Test_WeatherReport.cpp
@@ -6,19 +6,19 @@
 #include "Test_WeatherReport.h"
 
 void TestRainy() {
-    SensorStub sensor(26, 70, 72, 52);
-    std::string report = Report(sensor);
+    SensorStub sensor;
+    string report = Report(sensor);
     std::cout << report << std::endl;
-    assert(report == "Alert, Stormy with heavy rain");
+    assert(report.find("rain") != string::npos);
 }
 
 void TestHighPrecipitationAndLowWindspeed() {
     // This instance of stub needs to be different-
     // to give high precipitation (>60) and low wind-speed (<50)
-    SensorStub sensor(30, 70, 70, 40);
+    SensorStubHighPrecipitationLowWindSpeed sensor;
 
     // strengthen the assert to expose the bug
     // (function returns Sunny day, it should predict rain)
-    std::string report = Report(sensor);
-    assert(report == "Partly cloudy");
+    string report = Report(sensor);
+    assert(report.find("rain") != string::npos);
 }
--- WeatherReportGenerator.cpp
+++ WeatherReportGenerator.cpp
@@ -1,21 +1,16 @@
 #include "WeatherReportGenerator.h"
+#include <string>
 
-std::string Report(const IWeatherSensor& sensor) {
+string Report(const IWeatherSensor& sensor) {
     int precipitation = sensor.Precipitation();
-    int windSpeed = sensor.WindSpeedKMPH();
-    double temperature = sensor.TemperatureInC();
+    // precipitation < 20 is a sunny day
+    string report = "Sunny day";
 
-    if (temperature > 25) {
-        if (precipitation >= 60) {
-            if (windSpeed > 50) {
-                return "Alert, Stormy with heavy rain";
-            } else {
-                return "Rainy day";
-            }
-        } else if (precipitation >= 20) {
-            return "Partly cloudy";
-        }
+    if (sensor.TemperatureInC() > 25) {
+        if (precipitation >= 20 && precipitation < 60)
+            report = "Partly cloudy";
+        else if (sensor.WindSpeedKMPH() > 50)
+            report = "Alert, Stormy with heavy rain";
     }
-
-    return "Sunny day";
+    return report;
 }
